862e1c4825e05b3e35448de3982d3d82ecdd7a64
JeampierSegarra <jeampier690@gmail.com>
2024-06-28 14:19:49 -0500

    Ejercicio3

--- src/main/java/Ejercicios/Ejercicio3.java
+++ src/main/java/Ejercicios/Ejercicio3.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Ejercicios;
 
 import Framerwork.MapReduce;
@@ -9,34 +5,36 @@ import Framerwork.NodoMap;
 import Framerwork.Tarea;
 import Framerwork.ParClaveValor;
 import java.util.ArrayList;
-
 /**
  *
- * @author andjo
+ * @author Jeampier Segarra
  */
 public class Ejercicio3 {
- 
-    
-     static class map1 implements NodoMap {
-        /**
+
+    static class MapHourAccesses implements NodoMap {
+          /**
          * Guarda en la lista las horas.
          * @param elemento ParClaveValor
          * @param output  .
          */
         @Override
         public void Map(ParClaveValor elemento, ArrayList<ParClaveValor> output) {
-            String[] words = ((elemento.getValor())).toString().split(" ");
-            for (String wordss : words) {
-                if (wordss.startsWith("[") && wordss.endsWith("]")) {
-                    String hora = wordss.split(":")[1];
-                    output.add(new ParClaveValor(hora, 1));
+            try {
+                String[] logParts = ((String) elemento.getValor()).split(" ");
+                for (String part : logParts) {
+                    if (part.startsWith("[") && part.contains(":")) {
+                        String hour = part.split(":")[1];
+                        output.add(new ParClaveValor(hour, 1));
+                    }
                 }
+            } catch (Exception e) {
+                System.err.println("Error procesando la línea del log: " + elemento.getValor());
             }
         }
     }
 
-    static class reduce1 implements MapReduce {
-        /**
+    static class ReduceHourAccesses implements MapReduce {
+          /**
          * Cuenta las horas
          *
          * @param elemento ParClaveValor
@@ -44,27 +42,22 @@ public class Ejercicio3 {
          */
         @Override
         public void reduce(ParClaveValor elemento, ArrayList<ParClaveValor> output) {
-            ArrayList<Integer> list = (ArrayList<Integer>) elemento.getValor();
-            int count = 0;
-            for (Integer item : list) {
-                count += item;
+            ArrayList<Integer> counts = (ArrayList<Integer>) elemento.getValor();
+            int totalAccesses = 0;
+            for (Integer count : counts) {
+                totalAccesses += count;
             }
-
-            output.add(new ParClaveValor(elemento.getClave(), count));
+            output.add(new ParClaveValor(elemento.getClave(), totalAccesses));
         }
     }
-
 
     public static void main(String[] args) {
         Tarea t = new Tarea();
         t.setInputFile("weblog.txt");
         t.setOutputfile("Ejercicio3.txt");
         t.setNode(52);
-        t.setMapFunction(new map1());
-        t.setReduceFunction(new reduce1());
+        t.setMapFunction(new MapHourAccesses());
+        t.setReduceFunction(new ReduceHourAccesses());
         t.run();
     }
-    
-    
-    
 }
